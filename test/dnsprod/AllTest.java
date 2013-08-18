8a676ee1bbe28985998f55a05bfdac300c5ff46b
SunYiJun <dusunjun@126.com>
2013-08-18 18:19:37 +0800

    ! Add test case for IPUtil.

---
+++ test/dnsprod/AllTest.java
@@ -0,0 +1,15 @@
+package dnsprod;
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+/**
+ * @author yijun.sun
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ TestIPUtil.class })
+public abstract class AllTest {
+
+}
